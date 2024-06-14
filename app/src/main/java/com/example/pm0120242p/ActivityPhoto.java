23d97ff50e336f6dbc0135b19283963ebde766a1
Dell <ricardo.lagoshn@gmail.com>
2024-06-13 21:00:39 -0600

    Cambios al 13/06/2024

---
+++ app/src/main/java/com/example/pm0120242p/ActivityPhoto.java
@@ -0,0 +1,112 @@
+package com.example.pm0120242p;
+
+import android.Manifest;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.graphics.Bitmap;
+import android.media.Image;
+import android.os.Bundle;
+import android.provider.MediaStore;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.Toast;
+
+import androidx.activity.EdgeToEdge;
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+public class ActivityPhoto extends AppCompatActivity {
+    static final int peticion_acceso_camara = 101;
+    static final int peticion_captura_imagen = 102;
+    ImageView ObjectoImagen;
+    Button btncaptura;
+    String PathImagen;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_photo);
+
+
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return insets;
+        });
+
+        ObjectoImagen = (ImageView) findViewById(R.id.imageView);
+        btncaptura = (Button) findViewById(R.id.btntakefoto);
+
+        btncaptura.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Permisos();
+            }
+        });
+
+
+    }
+
+    private void Permisos()
+    {
+        if(ContextCompat.checkSelfPermission(getApplicationContext(), Manifest.permission.CAMERA) !=
+                PackageManager.PERMISSION_GRANTED)
+        {
+            ActivityCompat.requestPermissions(this,
+                    new String [] {Manifest.permission.CAMERA},
+                    peticion_acceso_camara);
+        }
+        else
+        {
+            TomarFoto();
+        }
+    }
+
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+
+        if(requestCode == peticion_acceso_camara)
+        {
+            if (grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED)
+            {
+                TomarFoto();
+            }
+            else
+            {
+                Toast.makeText(getApplicationContext(), "Acceso Denegado", Toast.LENGTH_LONG).show();
+            }
+        }
+    }
+
+    private void TomarFoto()
+    {
+        Intent intent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+
+        if(intent.resolveActivity(getPackageManager())!= null)
+        {
+            startActivityForResult(intent,  peticion_captura_imagen);
+        }
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        if(requestCode ==  peticion_captura_imagen)
+        {
+            Bundle extras = data.getExtras();
+            Bitmap imagen = (Bitmap) extras.get("data");
+            ObjectoImagen.setImageBitmap(imagen);
+        }
+    }
+}
