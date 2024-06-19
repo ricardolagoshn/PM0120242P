6dcb3f3b38deadffa6209cf91bfb2a50f17a065f
Dell <ricardo.lagoshn@gmail.com>
2024-06-18 20:56:49 -0600

    Cambios al 18/06/2024

--- app/src/main/java/com/example/pm0120242p/ActivityPhoto.java
+++ app/src/main/java/com/example/pm0120242p/ActivityPhoto.java
@@ -5,8 +5,12 @@ import android.content.Intent;
 import android.content.pm.PackageManager;
 import android.graphics.Bitmap;
 import android.media.Image;
+import android.net.Uri;
 import android.os.Bundle;
+import android.os.Environment;
 import android.provider.MediaStore;
+import android.util.Base64;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.ImageView;
@@ -18,9 +22,16 @@ import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.app.ActivityCompat;
 import androidx.core.content.ContextCompat;
+import androidx.core.content.FileProvider;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
+
+import java.io.ByteArrayOutputStream;
+import java.io.File;
+import java.io.IOException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
 
 public class ActivityPhoto extends AppCompatActivity {
     static final int peticion_acceso_camara = 101;
@@ -28,6 +39,7 @@ public class ActivityPhoto extends AppCompatActivity {
     ImageView ObjectoImagen;
     Button btncaptura;
     String PathImagen;
+    String currentPhotoPath, image64;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -102,11 +114,74 @@ public class ActivityPhoto extends AppCompatActivity {
     protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
         super.onActivityResult(requestCode, resultCode, data);
 
-        if(requestCode ==  peticion_captura_imagen)
+        if(requestCode ==  peticion_captura_imagen && resultCode == RESULT_OK)
         {
-            Bundle extras = data.getExtras();
-            Bitmap imagen = (Bitmap) extras.get("data");
-            ObjectoImagen.setImageBitmap(imagen);
+            if( data != null)
+            {
+                Bundle extras = data.getExtras();
+
+                if(extras != null)
+                {
+                    Bitmap imagen = (Bitmap) extras.get("data");
+                    ObjectoImagen.setImageBitmap(imagen);
+                    image64 = ConvertImageBase64(imagen);
+
+                    Log.i("Imagen ", image64);
+
+                }
+
+            }
         }
     }
+
+
+    private File createImageFile() throws IOException {
+        // Create an image file name
+        String timeStamp = new SimpleDateFormat("yyyyMMdd_HHmmss").format(new Date());
+        String imageFileName = "JPEG_" + timeStamp + "_";
+        File storageDir = getExternalFilesDir(Environment.DIRECTORY_PICTURES);
+        File image = File.createTempFile(
+                imageFileName,  /* prefix */
+                ".jpg",         /* suffix */
+                storageDir      /* directory */
+        );
+
+        // Save a file: path for use with ACTION_VIEW intents
+        currentPhotoPath = image.getAbsolutePath();
+        return image;
+    }
+
+    private void dispatchTakePictureIntent() {
+        Intent takePictureIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+        // Ensure that there's a camera activity to handle the intent
+        if (takePictureIntent.resolveActivity(getPackageManager()) != null) {
+            // Create the File where the photo should go
+            File photoFile = null;
+            try {
+                photoFile = createImageFile();
+            } catch (IOException ex) {
+                // Error occurred while creating the File
+
+            }
+            // Continue only if the File was successfully created
+            if (photoFile != null) {
+                Uri photoURI = FileProvider.getUriForFile(this,
+                        "com.example.pmo120232p.fileprovider",
+                        photoFile);
+                takePictureIntent.putExtra(MediaStore.EXTRA_OUTPUT, photoURI);
+                startActivityForResult(takePictureIntent, peticion_captura_imagen);
+            }
+        }
+    }
+
+    private String ConvertImageBase64(Bitmap bitmap)
+    {
+        ByteArrayOutputStream byteImage = new ByteArrayOutputStream();
+        bitmap.compress(Bitmap.CompressFormat.JPEG, 50, byteImage);
+
+        byte[] byteArray = byteImage.toByteArray();
+        return android.util.Base64.encodeToString(byteArray, Base64.DEFAULT);
+    }
+
 }
+
